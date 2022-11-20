1ed64fa5d07c2d4b25d18f6ac69b7d492097ed6f
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-11-21 01:00:02 +0200

    Project model described

---
+++ Lab5.DAL/Models/Project.cs
@@ -0,0 +1,8 @@
+namespace Lab5.DAL.Models;
+
+public class Project
+{
+    public int Id { get; set; }
+    public ICollection<User>? Team { get; set; } = null;
+    public ICollection<Task>? ProjectTasks { get; set; } = null; 
+}
