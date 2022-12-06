f084e707f7ab831c864ca23e9cbc849223eb7875
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-06 16:09:37 +0200

    Added DataAnnotations

--- Lab5.DAL/Entities/Project.cs
+++ Lab5.DAL/Entities/Project.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Lab5.DAL.Entities;
 
 public class Project
@@ -7,9 +9,12 @@ public class Project
         Users = new List<User>();
         Tasks = new List<Task>();
     }
-
+    
+    [Required]
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Project name must be specified")]
+    [StringLength(50, MinimumLength = 5, ErrorMessage = "Name must have length from 5 to 50 symbols")]
     public string ProjectName { get; set; }
     public ICollection<User>? Users { get; set; }
     public ICollection<Task>? Tasks { get; set; }
--- Lab5.DAL/Entities/User.cs
+++ Lab5.DAL/Entities/User.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Lab5.DAL.Entities;
 
 public class User
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "Name must be specified")]
+    [StringLength(50, MinimumLength = 5, ErrorMessage = "Name must have length from 5 to 50 symbols")]
     public string Name { get; set; } = "";
 
     public Task? CurrentTask { get; set; }
