6a508f1784bdad026bf1cb988bf370fe31e293bb
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-06 23:21:42 +0200

    UserService interface

---
+++ Lab5.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,22 @@
+using Lab5.BL.DTO;
+using Lab5.DAL.Entities;
+using Lab5.DAL.UnitOfWork;
+using Task = System.Threading.Tasks.Task;
+
+namespace Lab5.BL.Interfaces;
+
+public interface IUserService
+{
+    UnitOfWork Data { get; }
+    User CreateUser(CurrentUser newUser);
+    User GetUserById(int userId);
+    void DeleteUser(int userId);
+    void DeleteUser(User user);
+    void UpdateUser(CurrentUser newData);
+    void SetBusyness(int userId, bool isBusy);
+    void SetBusyness(User user, bool isBusy);
+    void AssignTask(User user, Task task);
+    void AssignTask(int userId, int taskId);
+    void ClearTask(User user);
+    void ClearTask(int userId);
+}
