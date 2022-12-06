95107eb637e25f7c7d4fdaf517b37f2643bc26f1
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-06 23:21:02 +0200

    Simple Business logic DTOs

---
+++ Lab5.BLL/DTO/CurrentProject.cs
@@ -0,0 +1,6 @@
+namespace Lab5.BL.DTO;
+
+public class CurrentProject
+{
+    public string Name = "";
+}
---
+++ Lab5.BLL/DTO/CurrentTask.cs
@@ -0,0 +1,9 @@
+namespace Lab5.BL.DTO;
+
+public class CurrentTask
+{
+    public string Name = "";
+    public string Description = "";
+    public bool Priority = false;
+    public string Status = "created";
+}
