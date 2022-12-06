f3a67fc76016db587a2194b099b18da39c77c9ea
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-07 00:54:35 +0200

    Change TaskDto by adding instances

--- Lab5.BLL/DTO/TaskDto.cs
+++ Lab5.BLL/DTO/TaskDto.cs
@@ -2,8 +2,8 @@ namespace Lab5.BLL.DTO;
 
 public class TaskDto
 {
-    public string Name = "";
-    public string Description = "";
-    public bool Priority = false;
-    public string Status = "created";
+    public readonly string Name = "";
+    public readonly string Description = "";
+    public readonly bool Priority = false;
+    public readonly string Status = "created";
 }
