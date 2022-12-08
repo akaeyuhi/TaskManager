43f3f6ab1e66857d28b65a01db527df5baae920f
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-08 02:38:05 +0200

    Fixed interface

--- Lab5.BLL/Interfaces/IUserService.cs
+++ Lab5.BLL/Interfaces/IUserService.cs
@@ -1,6 +1,6 @@
 using Lab5.BLL.DTO;
 using Lab5.DAL.Entities;
-using Task = System.Threading.Tasks.Task;
+using Task = Lab5.DAL.Entities.Task;
 
 namespace Lab5.BLL.Interfaces;
 
@@ -11,11 +11,12 @@ public interface IUserService
     IEnumerable<User> GetAllUsers();
     void DeleteUser(int userId);
     void DeleteUser(User user);
-    void UpdateUser(UserDto newData);
+    void UpdateUser(User user, UserDto newData);
+    void UpdateUser(int userId, UserDto newData);
     void SetBusyness(int userId, bool isBusy);
     void SetBusyness(User user, bool isBusy);
     void AssignTask(User user, Task task);
-    void AssignTask(int userId, int taskId);
+    void AssignTask(int userId, Task task);
     void ClearTask(User user);
     void ClearTask(int userId);
 }
