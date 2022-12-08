b3a72e9aab2228def39d01fac48b807e4f0d7ad6
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-08 02:49:28 +0200

    Implemented ProjectService class

---
+++ Lab5.BLL/Services/ProjectService.cs
@@ -0,0 +1,161 @@
+using Lab5.BLL.DTO;
+using Lab5.BLL.Infrastructure.Exceptions;
+using Lab5.BLL.Interfaces;
+using Lab5.DAL.Entities;
+using Lab5.DAL.Interfaces;
+using Task = Lab5.DAL.Entities.Task;
+
+namespace Lab5.BLL.Services;
+
+public class ProjectService: IProjectService
+{
+    private readonly IUnitOfWork _data;
+
+    public ProjectService(IUnitOfWork context)
+    {
+        _data = context;
+    }
+    
+    public Project CreateProject(ProjectDto projectDto)
+    {
+        var project = new Project
+        {
+            ProjectName = projectDto.Name,
+        };
+        try
+        {
+            _data.Projects.Add(project);
+            _data.Save();
+        }
+        catch (Exception ex)
+        {
+            throw new ProjectServiceException(ex.Message);
+        }
+
+        return project;
+    }
+
+    public Project GetProjectById(int projectId)
+    {
+        var project = _data.Projects.GetById(projectId);
+        if (project == null) throw new ProjectServiceException("Project not found in DB");
+        return project;
+    }
+
+    public IEnumerable<Project> GetAllProjects()
+    {
+        return _data.Projects.GetAll();
+    }
+
+    public void AddUsers(int projectId, IEnumerable<User> users)
+    {
+        var project = GetProjectById(projectId);
+        try
+        {
+            foreach (var user in users)
+            {
+                project.Users.Add(user);
+            }
+
+            _data.Projects.Update(project);
+            _data.Save();
+        }
+        catch (Exception ex)
+        {
+            throw new ProjectServiceException(ex.Message);
+        }
+        
+    }
+
+    public void AddTasks(int projectId, IEnumerable<Task> tasks)
+    {
+        var project = GetProjectById(projectId);
+        try
+        {
+            foreach (var task in tasks)
+            {
+                project.Tasks.Add(task);
+            }
+
+            _data.Projects.Update(project);
+            _data.Save();
+        }
+        catch (Exception ex)
+        {
+            throw new ProjectServiceException(ex.Message);
+        }
+    }
+
+    public void AddUsers(int projectId, User user)
+    {
+        var project = GetProjectById(projectId);
+        try
+        {
+            project.Users.Add(user);
+            _data.Projects.Update(project);
+            _data.Save();
+        }
+        catch (Exception ex)
+        {
+            throw new ProjectServiceException(ex.Message);
+        }
+    }
+
+    public void AddTasks(int projectId, Task task)
+    {
+        var project = GetProjectById(projectId);
+        try
+        {
+            project.Tasks.Add(task);
+            _data.Projects.Update(project);
+            _data.Save();
+        }
+        catch (Exception ex)
+        {
+            throw new ProjectServiceException(ex.Message);
+        }
+    }
+
+    public void DeleteProject(int projectId)
+    {
+        try
+        {
+            _data.Projects.Delete(projectId);
+            _data.Save();
+        }
+        catch (Exception ex)
+        {
+            throw new UserServiceException(ex.Message);
+        }
+    }
+
+    public void ClearTasks(int projectId)
+    {
+        var project = GetProjectById(projectId);
+        try
+        {
+            project.Tasks.Clear();
+            _data.Projects.Update(project);
+            _data.Save();
+        }
+        catch (Exception ex)
+        {
+            throw new ProjectServiceException(ex.Message);
+        }
+    }
+
+    public void ClearUsers(int projectId)
+    {
+        var project = GetProjectById(projectId);
+        try
+        {
+            project.Users.Clear();
+            _data.Projects.Update(project);
+            _data.Save();
+        }
+        catch (Exception ex)
+        {
+            throw new ProjectServiceException(ex.Message);
+        }
+    }
+}
