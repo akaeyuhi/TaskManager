bd4d87e45d7acc99c38e8c9b37c8fe630ac3ed37
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-12 19:20:24 +0200

    Added DI to startup

--- Lab5.PL/Program.cs
+++ Lab5.PL/Program.cs
@@ -1,8 +1,30 @@
+using Lab5.BLL.Infrastructure.Injections;
+using Lab5.BLL.Interfaces;
+using Lab5.BLL.Services;
+using Lab5.DAL.EF;
+using Lab5.DAL.Interfaces;
+using Lab5.DAL.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Ninject;
+using Ninject.Web.WebApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ManagerContext>(options => {
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<ITaskService, TaskService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IProjectService, ProjectService>();
+
+// var kernel = new StandardKernel(new BusinessBindings());
+// var dependencyResolver = new NinjectDependencyResolver(kernel);
+// dependencyResolver.BeginScope();
 
 var app = builder.Build();
 
@@ -11,6 +33,8 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -23,6 +47,5 @@ app.MapControllerRoute(
     pattern: "{controller}/{action=Index}/{id?}");
 
 app.MapFallbackToFile("index.html");
-;
 
 app.Run();
