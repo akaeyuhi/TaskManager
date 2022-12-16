2b32b76ee840d70e16f3cad0adbe55aa4e8f810c
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-16 23:41:32 +0200

    Dtos to get data from body

---
+++ Lab5.PL/DTO/UserIdDto.cs
@@ -0,0 +1,6 @@
+namespace Lab5.PL.DTO;
+
+public class UserIdDto
+{
+    public int UserId { get; set; }
+}
---
+++ Lab5.PL/DTO/UserIdsDto.cs
@@ -0,0 +1,6 @@
+namespace Lab5.PL.DTO;
+
+public class UserIdsDto
+{
+    public IEnumerable<int> UserIds { get; set; }
+}
