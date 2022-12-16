3c9a3826d0b25498d796161afd8bcdbe2f9fcbae
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-16 20:17:32 +0200

    Implemented UserController.cs

---
+++ Lab5.PL/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab5.BLL.DTO;
+using Lab5.BLL.Interfaces;
+using Lab5.DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab5.PL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        
+        
+        // GET: api/User
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_userService.GetAllUsers());
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "GetUser")]
+        public IActionResult Get(int id)
+        {
+            var user = _userService.GetUserById(id);
+            return user == null ? NotFound() : Ok(user);
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public IActionResult Post([FromBody] UserDto value)
+        {
+            var createdUser = _userService.CreateUser(value);
+            return CreatedAtAction(nameof(Post), createdUser);
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UserDto value)
+        {
+            _userService.UpdateUser(id, value);
+            return Ok();
+        }
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userService.DeleteUser(id);
+            return Ok();
+        }
+    }
+}
