69dad1dbf64f81ab1eaaad56e6d9f69b39fee106
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-17 01:18:15 +0200

    Code reorganize

---
+++ Lab5.PL/ClientApp/src/Utils/AppRoutes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Counter} from '../Pages/Counter';
+import {FetchData} from '../Pages/FetchData';
+import {Home} from '../Pages/Home';
+
+const AppRoutes = [
+    {
+        index: true,
+        element: <Home/>
+    },
+    {
+        path: '/counter',
+        element: <Counter/>
+    },
+    {
+        path: '/fetch-data',
+        element: <FetchData/>
+    }
+];
+
+export default AppRoutes;
