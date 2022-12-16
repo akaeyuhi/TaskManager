88a290e8beccac95f213f6deca638e6d25695a83
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-17 01:01:39 +0200

    Enabled swagger in non-production

--- Lab5.PL/Program.cs
+++ Lab5.PL/Program.cs
@@ -26,13 +26,13 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 
 app.MapControllerRoute(
