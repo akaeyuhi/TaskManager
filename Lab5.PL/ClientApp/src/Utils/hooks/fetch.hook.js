0d876697e5cf2668a37a39496fa7dfdf903bed0d
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-19 23:38:39 +0200

    Fetch hook to make code more clear

---
+++ Lab5.PL/ClientApp/src/Utils/hooks/fetch.hook.js
@@ -0,0 +1,27 @@
+import {useEffect, useState} from 'react';
+
+export const useFetch = (url, options = {}) => {
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState(null);
+    const [abort, setAbort] = useState(() => {});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const abortController = new AbortController();
+                const signal = abortController.signal;
+                setAbort(abortController.abort);
+                const res = await fetch(url, {...options, signal});
+                const json = await res.json();
+                setResponse(json);
+            } catch (error) {
+                setError(error);
+            }
+        };
+        fetchData().then();
+        return () => {
+            abort();
+        };
+    }, []);
+    return { response, error, abort };
+};
