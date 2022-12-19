751782f2df56161c844d523a574885d672a9b453
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-19 23:39:16 +0200

    Refactored to use fetch hook

--- Lab5.PL/ClientApp/src/Pages/Projects.js
+++ Lab5.PL/ClientApp/src/Pages/Projects.js
@@ -1,41 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Alert, Spinner} from 'reactstrap';
 import ProjectCard from '../components/Projects/ProjectCard';
+import {useFetch} from '../Utils/hooks/fetch.hook';
 
 const Projects = () => {
     const [projects, setProjects] = useState(null);
-    const fetchData = async () => {
-        try {
-            const result = await fetch('api/Project');
-            setProjects(await result.json());
-            console.log(projects);
-        } catch (e) {
-            console.error(e);
-        }
-    };
-
-    useEffect(() => {
-        fetchData().then();
-    }, []);
+    const { response, error } = useFetch('api/Project/');
+    if(!response) return <Spinner />;
+    else if(error) return console.error(error);
+    setProjects(response);
 
     return (
-        <>
-            {projects === null ? <Spinner /> :
-                <div className="container">
-                    <h1>Projects page</h1>
-                    <div className="flex mt-6">
-                        {projects.length ?
-                            projects.map((project, idx) => <ProjectCard key={idx} project={project} />) :
-                            <Alert color="primary">
+        <div className="container">
+            <h1>Projects page</h1>
+            <div className="flex mt-6">
+                {projects.length ?
+                    projects.map((project, idx) => <ProjectCard key={idx} project={project} />) :
+                    <Alert color="primary">
                                 No projects yet...
-                            </Alert>
-                        }
-                    </div>
-                </div>
-            }
-        </>
-
-
+                    </Alert>
+                }
+            </div>
+        </div>
     );
 };
 
--- Lab5.PL/ClientApp/src/Pages/SingleProject.js
+++ Lab5.PL/ClientApp/src/Pages/SingleProject.js
@@ -1,9 +1,10 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {useCallback, useState} from 'react';
 import {Alert, CardGroup, Spinner} from 'reactstrap';
 import {useParams} from 'react-router-dom';
 import UserCard from '../components/SingleProject/UserCard';
 import TaskCard from '../components/SingleProject/TaskCard';
 import DeleteModal from '../components/SingleProject/DeleteModal';
+import {useFetch} from '../Utils/hooks/fetch.hook';
 
 const SingleProject = () => {
     const [project, setProject] = useState(null);
@@ -14,14 +15,11 @@ const SingleProject = () => {
     });
     const params = useParams();
 
-    const fetchData = async () => {
-        try {
-            const result = await fetch(`api/Project/${params.id}`);
-            setProject(await result.json());
-        } catch (e) {
-            console.error(e);
-        }
-    };
+    const { response, error } = useFetch(`api/Project/${params.id}`);
+    if(!response) return <Spinner />;
+    else if(error) return console.error(error);
+    setProject(response);
+
     const deleteCallback = useCallback((deleteType, deleteId) => {
         setShowModal((prevState => ({
             isShown: !prevState.isShown,
@@ -41,46 +39,36 @@ const SingleProject = () => {
     }, [project, showModal]);
 
 
-    useEffect(() => {
-        fetchData().then();
-    }, []);
-
     return (
-        <>
-            {
-                project === null ? <Spinner /> :
-                    <div className="container">
-                        <h1>{project.projectName}</h1>
-                        <h2 className="mt-2">Users:</h2>
-                        <CardGroup>
-                            {
-                                project.users.length ?
-                                    project.users.map((user, idx) => <UserCard user={user} key={idx} callback={deleteCallback} />) :
-                                    <Alert color="primary">
+        <div className="container">
+            <h1>{project.projectName}</h1>
+            <h2 className="mt-2">Users:</h2>
+            <CardGroup>
+                {
+                    project.users.length ?
+                        project.users.map((user, idx) => <UserCard user={user} key={idx} callback={deleteCallback} />) :
+                        <Alert color="primary">
                                         There is no users.
-                                    </Alert>
-                            }
-                        </CardGroup>
-                        <h2 className="mt-2">Tasks:</h2>
-                        <CardGroup>
-                            {
-                                project.tasks.length ?
-                                    project.tasks.map((task, idx) => <TaskCard task={task} key={idx} callback={deleteCallback}/>) :
-                                    <Alert color="primary">
+                        </Alert>
+                }
+            </CardGroup>
+            <h2 className="mt-2">Tasks:</h2>
+            <CardGroup>
+                {
+                    project.tasks.length ?
+                        project.tasks.map((task, idx) => <TaskCard task={task} key={idx} callback={deleteCallback}/>) :
+                        <Alert color="primary">
                                         There is no tasks.
-                                    </Alert>
-                            }
-                        </CardGroup>
-                        <DeleteModal isOpen={showModal.isShown} toggleFunc={deleteCallback} deleteObject={{
-                            currentProjectId: +params.id,
-                            deleteType: showModal.deleteType,
-                            deleteId: showModal.deleteId,
-                        }
-                        } afterDeleteCb={clearCallback}/>
-                    </div>
+                        </Alert>
+                }
+            </CardGroup>
+            <DeleteModal isOpen={showModal.isShown} toggleFunc={deleteCallback} deleteObject={{
+                currentProjectId: +params.id,
+                deleteType: showModal.deleteType,
+                deleteId: showModal.deleteId,
             }
-        </>
-
+            } afterDeleteCb={clearCallback}/>
+        </div>
     );
 };
 
