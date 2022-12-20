4e4697a523d58924e6ef95ae5cae05c65b1c93d3
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-21 00:10:55 +0200

    Added AddUser modal

--- Lab5.PL/ClientApp/src/Pages/SingleProject.js
+++ Lab5.PL/ClientApp/src/Pages/SingleProject.js
@@ -1,21 +1,24 @@
 import React, {createContext, useCallback, useEffect, useState} from 'react';
-import {Alert, CardGroup, Spinner} from 'reactstrap';
+import {Alert, Button, CardGroup, Spinner} from 'reactstrap';
 import {useParams} from 'react-router-dom';
 import UserCard from '../components/SingleProject/UserCard';
 import TaskCard from '../components/SingleProject/TaskCard';
 import DeleteModal from '../components/SingleProject/DeleteModal';
+import AddUserModal from '../components/SingleProject/AddUserModal';
 export const ProjectContext = createContext(null);
 
 const SingleProject = () => {
     const [project, setProject] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [showModal, setShowModal] = useState({
+    const [showDeleteModal, setShowDeleteModal] = useState({
         isShown: false,
         deleteType: '',
         deleteId: null
     });
     const params = useParams();
+    const [showAddUserModal, setShowAddUserModal] = useState(false);
+    const toggleAddUsers = useCallback(() => setShowAddUserModal(prevState => !prevState), [showAddUserModal]);
 
     const fetchData = async () => {
         try {
@@ -28,29 +31,28 @@ const SingleProject = () => {
             setError(e);
         }
     };
-
-    useEffect(() => {
-        fetchData().then();
-    }, []);
-
     const deleteCallback = useCallback((deleteType, deleteId) => {
-        setShowModal((prevState => ({
+        setShowDeleteModal((prevState => ({
             isShown: !prevState.isShown,
             deleteType, deleteId
         })));
-    }, [showModal]);
-
+    }, [showDeleteModal]);
     const clearCallback = useCallback(() => {
-        if(showModal.deleteId !== null && showModal.deleteType !== '') {
-            const type = showModal.deleteType + 's';
-            const tempArray = project[type].filter(item => item.id !== showModal.deleteId);
+        if(showDeleteModal.deleteId !== null && showDeleteModal.deleteType !== '') {
+            const type = showDeleteModal.deleteType + 's';
+            const tempArray = project[type].filter(item => item.id !== showDeleteModal.deleteId);
             console.log(tempArray);
             setProject({
                 ...project,
                 [type]: tempArray
             });
         }
-    }, [project, showModal]);
+    }, [project, showDeleteModal]);
+
+
+    useEffect(() => {
+        fetchData().then();
+    }, []);
 
     if(loading) return <Spinner />;
     else if(error) return <Alert color="danger">{error}</Alert>;
@@ -58,7 +60,12 @@ const SingleProject = () => {
         <ProjectContext.Provider value={{project, setProject}}>
             <div className="container">
                 <h1>{project.projectName}</h1>
-                <h2 className="mt-2">Users:</h2>
+                <div className="d-flex justify-content-between mb-2">
+                    <h2 className="mt-2">Users:</h2>
+                    <Button color="warning" className="h-75" onClick={toggleAddUsers}>
+                        Add user
+                    </Button>
+                </div>
                 <CardGroup>
                     {
                         project.users.length ?
@@ -78,12 +85,12 @@ const SingleProject = () => {
                             </Alert>
                     }
                 </CardGroup>
-                <DeleteModal isOpen={showModal.isShown} toggleFunc={deleteCallback} deleteObject={{
-                    currentProjectId: +params.id,
-                    deleteType: showModal.deleteType,
-                    deleteId: showModal.deleteId,
+                <DeleteModal isOpen={showDeleteModal.isShown} toggleFunc={deleteCallback} deleteObject={{
+                    deleteType: showDeleteModal.deleteType,
+                    deleteId: showDeleteModal.deleteId,
                 }
                 } afterDeleteCb={clearCallback}/>
+                <AddUserModal modal={showAddUserModal} toggle={toggleAddUsers}/>
             </div>
         </ProjectContext.Provider>
     );
