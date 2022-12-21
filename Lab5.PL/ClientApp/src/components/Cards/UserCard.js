0353b5ad5f0fb1bfb3b99d309a64e66c0035f776
akaeyuhi <54174780+akaeyuhi@users.noreply.github.com>
2022-12-21 02:11:51 +0200

    Added link to project in card

--- Lab5.PL/ClientApp/src/components/Cards/UserCard.js
+++ Lab5.PL/ClientApp/src/components/Cards/UserCard.js
@@ -3,6 +3,7 @@ import {Button, Card, CardBody, CardText, CardTitle} from 'reactstrap';
 import PropTypes from 'prop-types';
 import DeleteUserModal from '../Modals/DeleteUserModal';
 import DeleteUserTaskModal from '../Modals/DeleteUserTaskModal';
+import {Link} from 'react-router-dom';
 
 const UserCard = ({ user }) => {
     const [userModal, setUserModal] = useState(false);
@@ -23,6 +24,12 @@ const UserCard = ({ user }) => {
                         `Current task: ${user.task.name}`
                         : 'User has no task'}
 
+                </CardText>
+                <CardText>
+                    {user.projectId ?
+                        <Link to={`/project/${user.projectId}`}>
+                           To users project
+                        </Link> : 'User doesn\'t participate in project'}
                 </CardText>
                 <div className="d-flex justify-content-between">
                     <Button color="danger" onClick={() => toggleUser()}>
@@ -50,7 +57,8 @@ UserCard.propTypes = {
             description: PropTypes.string,
             priority: PropTypes.bool,
             status: PropTypes.string
-        })
+        }),
+        projectId: PropTypes.number
     })
 };
 
